d774cd29ce3c82c830d0093660a80bc41aabd22b
kulagin <alex_kulagin@samgk.ru>
2023-11-05 11:52:32 +0400

    Update dtoAccountUpdate.cs

--- RegistrantApp/Shared/Dto/Accounts/dtoAccountUpdate.cs
+++ RegistrantApp/Shared/Dto/Accounts/dtoAccountUpdate.cs
@@ -1,12 +1,48 @@
-﻿namespace RegistrantApp.Shared.Dto.Accounts;
+﻿using RegistrantApp.Shared.Validators;
+
+namespace RegistrantApp.Shared.Dto.Accounts;
 
 public class dtoAccountUpdate
 {
     public long AccountID { get; set; }
-    public string FirstName { get; set; }
-    public string MiddleName { get; set; }
-    public string? LastName { get; set; }
-    public long PhoneNumber { get; set; }
-    public string? Password { get; set; }
+    private string firstName;
+
+    public string FirstName
+    {
+        get => firstName.ToUpper();
+        set => firstName = value.ToUpper();
+    }
+
+    private string middleName;
+
+    public string MiddleName
+    {
+        get => middleName.ToUpper();
+        set => middleName = value.ToUpper();
+    }
+
+    private string? lastName;
+
+    public string? LastName
+    {
+        get => lastName?.ToUpper();
+        set => lastName = value?.ToUpper();
+    }
+
+    private long phoneNumber;
+
+    public long PhoneNumber
+    {
+        get => phoneNumber;
+        set => phoneNumber = MyValidator.PhoneMinimalValidation(value!.ToString());
+    }
+
+    private string? password;
+
+    public string? Password
+    {
+        get => password;
+        set => password = MyValidator.CreateMD5(value!);
+    }
     public bool IsEmployee { get; set; }
 }
