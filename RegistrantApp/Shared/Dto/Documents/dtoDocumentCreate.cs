6f2d2927e55e6340cb608ff45b618f4364cee35e
kulagin <alex_kulagin@samgk.ru>
2023-11-05 12:53:24 +0400

    update dtoDocument

--- RegistrantApp/Shared/Dto/Documents/dtoDocumentCreate.cs
+++ RegistrantApp/Shared/Dto/Documents/dtoDocumentCreate.cs
@@ -2,11 +2,39 @@
 
 public class dtoDocumentCreate
 {
-    public string Title { get; set; }
-    public string? Seria { get; set; }
-    public string? Number { get; set; }
-    public string? Authority { get; set; }
+    private string title;
+
+    public string Title
+    {
+        get => title.ToUpper();
+        set => title = value.ToUpper();
+    }
+
+    private string? seria;
+
+    public string? Seria
+    {
+        get => seria?.ToUpper();
+        set => seria = value?.ToUpper();
+    }
+
+    private string? number;
+
+    public string? Number
+    {
+        get => number?.ToUpper();
+        set => number = value?.ToUpper();
+    }
+
+    private string? authority;
+
+    public string? Authority
+    {
+        get => authority?.ToUpper();
+        set => authority = value?.ToUpper();
+    }
+
     public DateOnly DateOfIssue { get; set; }
-    
+
     public long idAccount { get; set; }
 }
--- RegistrantApp/Shared/Dto/Documents/dtoDocumentUpdate.cs
+++ RegistrantApp/Shared/Dto/Documents/dtoDocumentUpdate.cs
@@ -3,9 +3,36 @@
 public class dtoDocumentUpdate
 {
     public long DocumentID { get; set; }
-    public string Title { get; set; }
-    public string? Seria { get; set; }
-    public string? Number { get; set; }
-    public string? Authority { get; set; }
+    private string title;
+
+    public string Title
+    {
+        get => title.ToUpper();
+        set => title = value.ToUpper();
+    }
+
+    private string? seria;
+
+    public string? Seria
+    {
+        get => seria?.ToUpper();
+        set => seria = value?.ToUpper();
+    }
+
+    private string? number;
+
+    public string? Number
+    {
+        get => number?.ToUpper();
+        set => number = value?.ToUpper();
+    }
+
+    private string? authority;
+
+    public string? Authority
+    {
+        get => authority?.ToUpper();
+        set => authority = value?.ToUpper();
+    }
     public DateOnly DateOfIssue { get; set; }
 }
