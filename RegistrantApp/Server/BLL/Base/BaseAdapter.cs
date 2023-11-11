67e210fdd70fdcba541776fad6c9c9be64019fd1
kulagin <alex_kulagin@samgk.ru>
2023-11-11 23:07:05 +0400

    Added page Login.razor

--- RegistrantApp/Server/BLL/Base/BaseAdapter.cs
+++ RegistrantApp/Server/BLL/Base/BaseAdapter.cs
@@ -1,7 +1,4 @@
-﻿using System.Globalization;
-using System.Reflection;
-using RegistrantApp.Server.Database.Base;
-using RegistrantApp.Shared.Database;
+﻿using RegistrantApp.Server.Database.Base;
 
 namespace RegistrantApp.Server.BLL.Base;
 
@@ -11,50 +8,4 @@ public class BaseAdapter
 
     protected BaseAdapter(RaContext ef)
         => _ef = ef;
-
-
-    /*protected async void AuditAdd(Account account, object entity)
-    {
-        var ownerEvent = $"{account.FirstName} {account.MiddleName.First()}. {account.LastName?.First()}.";
-
-        foreach (PropertyInfo propertyInfo in entity.GetType().GetProperties())
-        {
-            var curValue1 = _ef.Entry(entity).CurrentValues.TryGetValue<string>(propertyInfo.Name,out var curValue);
-            
-            var log = new Audit()
-            {
-                DateTimeEvent = DateTime.Now,
-                OwnerEvent = ownerEvent,
-                Object = entity!.ToString()!,
-                Action = "Создание объекта",
-                Property = propertyInfo.Name,
-                ValueBefore = "",
-                ValueAfter = curValue
-                
-            };
-            
-            await _ef.AddAsync(log);
-            await _ef.SaveChangesAsync();
-        }
-    }
-
-    protected async void RegisterEvent(Account account, string obj, string? action, string? property,
-        string? valueafter, string? valuebefore)
-    {
-        var ownerEvent = $"{account.FirstName} {account.MiddleName.First()}. {account.LastName?.First()}.";
-
-        var @event = new Audit()
-        {
-            DateTimeEvent = DateTime.Now,
-            OwnerEvent = ownerEvent,
-            Object = obj,
-            Action = action,
-            Property = property,
-            ValueBefore = valuebefore,
-            ValueAfter = valueafter
-        };
-
-        await _ef.AddAsync(@event);
-        await _ef.SaveChangesAsync();
-    }*/
 }
