918a765ded20636b33df3b800f0e485d2489a3d9
kulagin <alex_kulagin@samgk.ru>
2023-11-11 20:06:30 +0400

    Comments

--- RegistrantApp.ClientApi/RApi.cs
+++ RegistrantApp.ClientApi/RApi.cs
@@ -4,14 +4,41 @@ namespace RegistrantApp.ClientApi;
 
 public class RApi
 {
+    /// <summary>
+    /// Класс для работы с аккантами
+    /// </summary>
     public Accounts Accounts { get; private set; }
+    /// <summary>
+    /// Класс для работы с машинами от водителей(аккаунтов)
+    /// </summary>
     public Autos Autos { get; private set; }
+    /// <summary>
+    /// Класс для работы с контрагентами
+    /// </summary>
     public Contragents Contragents { get; private set; }
+    /// <summary>
+    /// Класс для работы с документами
+    /// </summary>
     public Documents Documents { get; private set; }
+    /// <summary>
+    /// Класс для работы с файлами
+    /// </summary>
     public Files Files { get; private set; }
+    /// <summary>
+    /// Класс для работы с деталями заказа
+    /// </summary>
     public OrderDetails OrderDetails { get; private set; }
+    /// <summary>
+    /// Класс для работы с заказами
+    /// </summary>
     public Orders Orders { get; private set; }
+    /// <summary>
+    /// Действия с учетной записью
+    /// </summary>
     public Security Security { get; private set; }
+    /// <summary>
+    /// Журналы аудита
+    /// </summary>
     public Audit Audit { get; private set; }
     
     public RApi(string connectionString)
